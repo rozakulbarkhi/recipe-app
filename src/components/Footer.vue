c533f59418ee910eb481a8f78fdaa1339b6a394e
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2024-11-06 23:15:35 +0700

    feat: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <div class="container mx-auto p-6 flex items-center justify-between">
+      <Logo />
+
+      <div>Built with ❤️</div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import Logo from "./Logo.vue";
+</script>
